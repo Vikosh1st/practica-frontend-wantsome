09294d1e39f5037fbe069985676b76793935405b
Victor Bondariu <victorbondariu@gmail.com>
2019-04-21 23:30:46 +0300

    Added tema JS 7 Register Form with Validations - partially done

--- JS-Training/Wantsome-JavaScript-Homeworks/Tema_JavaScript_7/Register Form with Validations Assignment/index.js
+++ JS-Training/Wantsome-JavaScript-Homeworks/Tema_JavaScript_7/Register Form with Validations Assignment/index.js
@@ -1,1 +1,33 @@
-console.log("Register Form with Validations Assignment")
+const validateForm = () => {
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let firstname = document.getElementById("firstname").value;
+    let lastname = document.getElementById("lastname").value;
+    let phone = document.getElementById("phone").value;
+    if (username === "") {
+        document.getElementById("usernameError").style.display = "block";
+    } else {
+        document.getElementById("usernameError").style.display = "none";
+    }
+    if (email === "") {
+        document.getElementById("emailError").style.display = "block";
+    } else {
+        document.getElementById("emailError").style.display = "none";
+    }
+    if (firstname === "") {
+        document.getElementById("firstnameError").style.display = "block";
+    } else {
+        document.getElementById("firstnameError").style.display = "none";
+    }
+    if (lastname === "") {
+        document.getElementById("lastnameError").style.display = "block";
+    } else {
+        document.getElementById("lastnameError").style.display = "none";
+    }
+    if (phone === "") {
+        document.getElementById("phoneError").style.display = "block";
+    } else {
+        document.getElementById("phoneError").style.display = "none";
+        alert("The form has been submitted, please check your email for further details");
+    }
+}
