1bbd218606fa4eb8848292c0618ac125c0b1c548
Victor Bondariu <victorbondariu@gmail.com>
2019-05-05 20:21:20 +0300

    Updated Tema_JavaScript_2_Function_types_&_Conditions to ES6

--- JS-Training/Wantsome-JavaScript-Homeworks/Tema_JavaScript_2_Function_types_&_Conditions/Tema_JavaScript_2_Function_types_&_Conditions.js
+++ JS-Training/Wantsome-JavaScript-Homeworks/Tema_JavaScript_2_Function_types_&_Conditions/Tema_JavaScript_2_Function_types_&_Conditions.js
@@ -6,9 +6,8 @@
 // 1. Scrieti o functie de tip named function care accepta ca argument un CNP si verifica sexul unei persoane. 
 // In urma apelului acestei functii, se va returna un text de forma "Persoana verificata este de sexul M/F".
 
-function verifySexFromCnp(CNP) {
-    var sexul = CNP.toString().charAt(0);
-
+function verifySexFromCnp (CNP) {
+    const sexul = CNP.toString().charAt(0);
     if ( sexul === "1" || sexul === "3" || sexul === "5" || sexul === "7" ) {
         return "Persoana verificata este de sexul Masculin";
     } else if ( sexul === "2" || sexul === "4" || sexul === "6" || sexul === "8") {
@@ -16,7 +15,7 @@ function verifySexFromCnp(CNP) {
     } else return "CNP-ul nu este valid";
 }
 
-console.log("Exercitiul 1:");
+console.log("Exercise 1:");
 console.log(verifySexFromCnp(1870330));
 console.log(verifySexFromCnp(2870330));
 console.log(verifySexFromCnp(3870330));
@@ -32,34 +31,34 @@ decide calificativul punctajului. Criteriile sunt urmatoarele:
 - 10 = A+
 Functia va returna un text de forma "Calificatul corespunzator punctajului [punctaj] este [calificativ]".*/
 
-var calificativCorespunzatorPunctajului = function(punctaj) {
-    if ( punctaj >= 1 && punctaj <= 10 ) { // Pentru ca punctajul sa fie cuprins intre 1 si 10
+const markMatchingTheSpecificGrade = (grade) => {
+    if ( grade >= 1 && grade <= 10 ) { // Pentru ca punctajul sa fie cuprins intre 1 si 10
         switch (true) { // Pentru a returna calificativul atribuit fiecarui punctaj
-            case punctaj >= 1 && punctaj <= 3:
-            calificativ = "E";
+            case grade >= 1 && grade <= 3:
+            mark = "E";
             break;
-            case punctaj > 3 && punctaj <= 6: 
-            calificativ = "D";
+            case grade > 3 && grade <= 6: 
+            mark = "D";
             break;
-            case punctaj > 6 && punctaj <= 8:
-            calificativ = "B";
+            case grade > 6 && grade <= 8:
+            mark = "B";
             break;
-            case punctaj === 9:
-            calificativ = "A";
+            case grade === 9:
+            mark = "A";
             break;
-            case punctaj === 10:
-            calificativ = "A+";
-        } return "Calificatul corespunzator punctajului " + punctaj + " este " + calificativ;
+            case grade === 10:
+            mark = "A+";
+        } return "Calificatul corespunzator punctajului " + grade + " este " + mark;
     } return "Punctajul trebuie sa fie cuprins intre valoarea 1 si 10 pentru a putea calcula calificativul";
 }
 
-console.log("Exercitiul 2:");
-console.log(calificativCorespunzatorPunctajului(2));
-console.log(calificativCorespunzatorPunctajului(4));
-console.log(calificativCorespunzatorPunctajului(7));
-console.log(calificativCorespunzatorPunctajului(9));
-console.log(calificativCorespunzatorPunctajului(10));
-console.log(calificativCorespunzatorPunctajului(20));
+console.log("Exercise 2:");
+console.log(markMatchingTheSpecificGrade(2));
+console.log(markMatchingTheSpecificGrade(4));
+console.log(markMatchingTheSpecificGrade(7));
+console.log(markMatchingTheSpecificGrade(9));
+console.log(markMatchingTheSpecificGrade(10));
+console.log(markMatchingTheSpecificGrade(20));
 
 // 3. Scrieti o functie care accepta ca argument numele marcii unei masini si returneaza un text de forma: 
 // "Marca [marca] se produce in [tara]". Trebuie sa aveti macar 6 cazuri. 
@@ -67,92 +66,91 @@ console.log(calificativCorespunzatorPunctajului(20));
 // Scrieti codul in 3 variante diferite.
 
 // Varianta 1
-function returneazaTaraFolosindDoarIfElse(marca) {
-    if (marca === "Dacia"){
-        return "Marca " + marca + " se produce in Romania."; 
+const returnCountryUsingIfElse = (brand) => {
+    if (brand === "Dacia"){
+        return "Marca " + brand + " se produce in Romania."; 
     } 
-    if (marca === "Seat") {
-        return "Marca " + marca + " se produce in Spania.";
+    if (brand === "Seat") {
+        return "Marca " + brand + " se produce in Spania.";
     }    
-    if (marca === "Fiat") {
-        return "Marca " + marca + " se produce in Italia.";
+    if (brand === "Fiat") {
+        return "Marca " + brand + " se produce in Italia.";
     } 
-    if (marca === "BMW") {
-        return "Marca " + marca + " se produce in Germania.";
+    if (brand === "BMW") {
+        return "Marca " + brand + " se produce in Germania.";
     } 
-    if (marca === "Audi") {
-        return "Marca " + marca + " se produce in Germania.";
+    if (brand === "Audi") {
+        return "Marca " + brand + " se produce in Germania.";
     } 
-    if (marca === "Mercedes") {
-        return "Marca " + marca + " se produce in Germania.";
+    if (brand === "Mercedes") {
+        return "Marca " + brand + " se produce in Germania.";
     }   
     else {
         return "Marca este necunoscuta.";
     }
 }
 
-console.log("Exercitiul 3:");
-console.log("Varianta 1:");
-console.log(returneazaTaraFolosindDoarIfElse("Seat"));
-console.log(returneazaTaraFolosindDoarIfElse("BMW"));
-console.log(returneazaTaraFolosindDoarIfElse("Ford"));
+console.log("Exercise 3:");
+console.log("Answer 1:");
+console.log(returnCountryUsingIfElse("Seat"));
+console.log(returnCountryUsingIfElse("BMW"));
+console.log(returnCountryUsingIfElse("Ford"));
 
-//Varianta 2
-
-var tara = function atribuieTaraFiecareiMarci(marca) {
-    if (marca === "Dacia") {
+//Varianta 2:
+const country = (brand) => {
+    if (brand === "Dacia") {
         return "Romania";
-    } else if (marca === "Seat") {
+    } else if (brand === "Seat") {
         return "Spania";
-    } else if (marca === "Fiat") {
+    } else if (brand === "Fiat") {
         return "Italia";
-    } else if (marca === "BMW") {
+    } else if (brand === "BMW") {
         return "Germania";
-    } else if (marca === "Audi") {
+    } else if (brand === "Audi") {
         return "Germania";
-    } else if (marca === "Mercedes") {
+    } else if (brand === "Mercedes") {
         return "Germania";
     }
 };
 
-function returneazaTaraFolosindElseIf(marca) {
-    if (tara(marca) === undefined) {
-        return "Marca este necunoscuta."
+const returnCountryUsingElseIf = (brand) => {
+    if (country(brand) === undefined) {
+        return "Marca este necunoscuta.";
     }
-    return "Marca " + marca + " se produce in " + tara(marca) + ".";
+    return "Marca " + brand + " se produce in " + country(brand) + ".";
 }
 
-console.log("Varianta 2:");
-console.log(returneazaTaraFolosindElseIf("Fiat"));
-console.log(returneazaTaraFolosindElseIf("Audi"));
-console.log(returneazaTaraFolosindElseIf("Ford"));
+console.log("Answer 2:");
+console.log(returnCountryUsingElseIf("Fiat"));
+console.log(returnCountryUsingElseIf("Audi"));
+console.log(returnCountryUsingElseIf("Ford"));
 
 // Varianta 3
-function returneazaTaraFolosindSwitch(marca){
-    switch(marca) {
+const returnCountryUsingSwitch = (brand) => {
+    switch(brand) {
         case "Dacia":
-        return  "Marca " + marca + " se produce in Romania.";
+        return  "Marca " + brand + " se produce in Romania.";
         break;
         case "Seat":
-        return  "Marca " + marca + " se produce in Spania.";
+        return  "Marca " + brand + " se produce in Spania.";
         break;
         case "Fiat":
-        return  "Marca " + marca + " se produce in Italia.";
+        return  "Marca " + brand + " se produce in Italia.";
         break;
         case "BMW":
         case "Audi":
         case "Mercedes":
-        return  "Marca " + marca + " se produce in Germania.";
+        return  "Marca " + brand + " se produce in Germania.";
         break;
         default:
         return "Marca este necunoscuta.";
     }
 }
 
-console.log("Varianta 3:");
-console.log(returneazaTaraFolosindSwitch("Dacia"));
-console.log(returneazaTaraFolosindSwitch("Mercedes"));
-console.log(returneazaTaraFolosindSwitch("Ford"));
+console.log("Answer 3:");
+console.log(returnCountryUsingSwitch("Dacia"));
+console.log(returnCountryUsingSwitch("Mercedes"));
+console.log(returnCountryUsingSwitch("Ford"));
 
 // 4. Scrieti o functie de tip IIFE care afiseaza in consola: "Afisez, deci ma autoapelez". Veti crea un fisier javascript separat pe care il veti include intr-un fisier html. Verificarea se va face prin deschiderea paginii html in browser.
 // Rezolvarea exercitiului 4 se afla pe index.html, respectiv index.js, din acelasi folder.
