29b59a190780dffd12ee3e9fa1492c6ed66c2b3b
Victor Bondariu <victorbondariu@gmail.com>
2019-05-11 17:24:59 +0300

    Finished the Classwork exercises from 25/04

--- JS-Training/JS-Classwork/April/25-04-Thursday/Ex3/index.js
+++ JS-Training/JS-Classwork/April/25-04-Thursday/Ex3/index.js
@@ -7,45 +7,47 @@ const usersUrl = "https://jsonplaceholder.typicode.com/users";
 const todosUrl = "https://jsonplaceholder.typicode.com/todos"
 
 const todosCallback = (todos,users) => {
-    console.log(todos);
-    console.log(users);
+  console.log(todos);
+  console.log(users);
+  for (let i = 0; i < todos.length; i++) {
+    for (let j = 0; j < users.length; j++) {
+      if (todos[i].id === users[j].id) {
+        displayTitleInParagraph(users[j].name,users[j].email,todos[i].title);
+      }
+    }
+  }
+}
+
+const displayTitleInParagraph = (name,email,title) => {
+  let paragraph = document.createElement("p");
+  paragraph.textContent = name + " with the email address " + email + " needs to do " + title;
+  document.getElementById("app").appendChild(paragraph);
 }
 
 const usersCallback = (users) => {
-    getTodos(users, todosCallback);
+  getTodos(users, todosCallback);
 }
 
-const getUsers = (callback) =>{
+const getUsers = (callback) => {
   const requestUsers = new XMLHttpRequest();
-  requestUsers.onload = function() {
-    const UsersJson = requestUsers.response;
-    callback(UsersJson);
-  } 
-
   requestUsers.open("GET",usersUrl);
   requestUsers.responseType = "json";
   requestUsers.send();
-
+  requestUsers.onload = function() {
+    const UsersJson = requestUsers.response;
+    callback(UsersJson);
+  }
 }
 
 const getTodos = (users, callback) => {
   const requestTodos = new XMLHttpRequest();
+  requestTodos.open("GET",todosUrl);
+  requestTodos.responseType = "json";
+  requestTodos.send();
   requestTodos.onload = function() {
     const TodosJson = requestTodos.response;
     callback(TodosJson, users);
   }
-  requestTodos.open("GET",todosUrl);
-  requestTodos.responseType = "json";
-  requestTodos.send();
 }
 
 getUsers(usersCallback);
-
-
-// const showUserDetails = () => {
-//     const UserObj = getUsers;
-//     const TodosObj = getTodos();
-//     console.log(UserObj);
-// }
-
-// showUserDetails();
